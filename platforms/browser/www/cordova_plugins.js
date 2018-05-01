900a7667398803b84480138ba0f9a670c6240990
Rafi Priatna <stormrider136@gmail.com>
2018-05-01 20:15:15 +0700

    Runs Radio Framework7

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,62 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/browser/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
+        "id": "cordova-plugin-dialogs.notification_browser",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-streaming/www/radio.js",
+        "id": "cordova-plugin-streaming.RADIO",
+        "pluginId": "cordova-plugin-streaming",
+        "clobbers": [
+            "navigator.RADIO"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-statusbar": "1.0.1",
+    "cordova-plugin-inappbrowser": "1.7.1",
+    "cordova-plugin-dialogs": "1.3.3",
+    "cordova-plugin-streaming": "1.4.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/plugins/cordova-plugin-streaming/www/radio.js
@@ -0,0 +1,18 @@
+cordova.define("cordova-plugin-streaming.RADIO", function(require, exports, module) { var RADIO = {
+    initialize: function(successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, 'RadioPlugin', 'initialize', [ ]);
+    },
+
+    play: function(successCallback, failureCallback, url, singerName, songName) {
+        cordova.exec(successCallback, failureCallback, 'RadioPlugin', 'play', [ url, singerName, songName ]);
+    },
+
+    stop: function(successCallback, failureCallback) {
+        cordova.exec(successCallback, failureCallback, 'RadioPlugin', 'stop', [ ]);
+    }
+
+};
+
+module.exports = RADIO;
+
+});
